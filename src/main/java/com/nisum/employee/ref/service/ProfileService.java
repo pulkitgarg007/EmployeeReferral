b79a78c85893dbba5449bcdf3d69bc81e2cae737
vin4yak <vinayakprabhu92@gmail.com>
2015-05-22 14:16:44 +0530

    Implement Send Mail module using gmail smtp

--- src/main/java/com/nisum/employee/ref/service/ProfileService.java
+++ src/main/java/com/nisum/employee/ref/service/ProfileService.java
@@ -1,7 +1,16 @@
 package com.nisum.employee.ref.service;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Pattern;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -35,7 +44,6 @@ public class ProfileService {
 		Query query = new Query();
 		query.addCriteria(Criteria.where("emailId").regex(Pattern.compile(emailId, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE)));
 		List<Profile> candidateDetails = mongoOperations.find(query, Profile.class);
-		
 		return candidateDetails;
 	}
   
@@ -78,4 +86,49 @@ public class ProfileService {
 		mongoOperations.remove(profileDetail);
 		return profileDetail;
 	}
+  public String sendMail(String emailId, String jobcode){
+		
+      String to = emailId;
+
+      String from = "ositechportal@gmail.com";
+      final String username = "ositechportal@gmail.com";
+      final String password = "123osi123";
+
+      String host = "smtp.gmail.com";
+
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "587");
+
+      Session session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+         }
+      });
+
+      try {
+         Message message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.setRecipients(Message.RecipientType.TO,
+         InternetAddress.parse(to));
+
+         message.setSubject("OSI Technologies");
+         message.setText("OSI Recruitment Portal - Test! - Job Code: " + jobcode);
+         Transport.send(message);
+
+         System.out.println("Sent message successfully....");
+
+      } catch (MessagingException e) {
+            throw new RuntimeException(e);
+      }
+      return "Email Sent Successfully To: " + emailId;
+}
+
 }
